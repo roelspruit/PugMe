e455a7a2910e879e3593df3f3159f6442e96f4b3
Roel Spruit <roel@funda.nl>
2018-11-29 11:31:56 +0100

    no message

--- PugMe/AppDelegate.swift
+++ PugMe/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let builder = ImageViewBuilder(subreddit: "pugs", clientId: INSERT_REDDIT_CLIENT_ID_HERE)
+        let builder = ImageViewBuilder(subreddit: "pugs", clientId: "INSERT_REDDIT_CLIENT_ID_HERE")
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = builder.build()
