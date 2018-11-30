3d268f51715f9733e0ee4880bf0dfcde794b7261
Roel Spruit <roel@funda.nl>
2018-11-30 09:21:52 +0100

    Re-added snapshot tests

--- PugMeTests/ImageViewControllerTests.swift
+++ PugMeTests/ImageViewControllerTests.swift
@@ -14,51 +14,51 @@ import Cuckoo
 
 class ImageViewControllerTest: FBSnapshotTestCase {
     
-//    var mockPresenter: MockImageViewPresenting!
-//    var imageViewController: ImageViewController!
-//    
-//    override func setUp() {
-//        super.setUp()
-//        recordMode = false
-//        
-//        mockPresenter = MockImageViewPresenting()
-//        stub(mockPresenter) { (stub) in
-//            when(stub.viewDidLoad()).thenDoNothing()
-//            when(stub.tappedView()).thenDoNothing()
-//        }
-//        
-//        imageViewController = ImageViewController(nibName: nil, bundle: nil)
-//        imageViewController.presenter = mockPresenter
-//    }
-//    
-//    func testRefreshState() {
-//        let imageViewController = ImageViewController(nibName: nil, bundle: nil)
-//        imageViewController.showRefreshState()
-//        FBSnapshotVerifyView(imageViewController.view)
-//    }
-//    
-//    func testErrorState() {
-//        let imageViewController = ImageViewController(nibName: nil, bundle: nil)
-//        imageViewController.showError()
-//        FBSnapshotVerifyView(imageViewController.view)
-//    }
-//    
-//    func testImageState() {
-//        let image = UIImage(named: "pug", in: Bundle.init(for: ImageDownloaderTests.self), compatibleWith: nil)!
-//        let imageViewController = ImageViewController(nibName: nil, bundle: nil)
-//        imageViewController.showImage(image)
-//        FBSnapshotVerifyView(imageViewController.view)
-//    }
-//    
-//    func testViewDidLoadShouldCallPresenter() {
-//        imageViewController.viewDidLoad()
-//        verify(mockPresenter).viewDidLoad()
-//    }
-//    
-//    func testTappingViewShouldCallPresenter() {
-//        imageViewController.tapped()
-//        verify(mockPresenter).tappedView()
-//    }
+    var mockPresenter: MockImageViewPresenting!
+    var imageViewController: ImageViewController!
+    
+    override func setUp() {
+        super.setUp()
+        recordMode = false
+        
+        mockPresenter = MockImageViewPresenting()
+        stub(mockPresenter) { (stub) in
+            when(stub.viewDidLoad()).thenDoNothing()
+            when(stub.tappedView()).thenDoNothing()
+        }
+        
+        imageViewController = ImageViewController(nibName: nil, bundle: nil)
+        imageViewController.presenter = mockPresenter
+    }
+    
+    func testRefreshState() {
+        let imageViewController = ImageViewController(nibName: nil, bundle: nil)
+        imageViewController.showRefreshState()
+        FBSnapshotVerifyView(imageViewController.view)
+    }
+    
+    func testErrorState() {
+        let imageViewController = ImageViewController(nibName: nil, bundle: nil)
+        imageViewController.showError()
+        FBSnapshotVerifyView(imageViewController.view)
+    }
+    
+    func testImageState() {
+        let image = UIImage(named: "pug", in: Bundle.init(for: ImageDownloaderTests.self), compatibleWith: nil)!
+        let imageViewController = ImageViewController(nibName: nil, bundle: nil)
+        imageViewController.showImage(image)
+        FBSnapshotVerifyView(imageViewController.view)
+    }
+    
+    func testViewDidLoadShouldCallPresenter() {
+        imageViewController.viewDidLoad()
+        verify(mockPresenter).viewDidLoad()
+    }
+    
+    func testTappingViewShouldCallPresenter() {
+        imageViewController.tapped()
+        verify(mockPresenter).tappedView()
+    }
 }
 
 
